71905b9606b0c8851b391b16afe4dc262db7d1b4
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-08-08 13:44:51 -0300

    organiza los script por categorias

--- install.sh
+++ install.sh
@@ -5,7 +5,4 @@ ln -s $HOME/.config/dot/i3status       $HOME/.config/i3status
 ln -s $HOME/.config/dot/vim            $HOME/.config/vim             
 ln -s $HOME/.config/dot/user-dirs.dirs $HOME/.config/user-dirs.dirs  
 ln -s $HOME/.config/dot/mimeapps.list  $HOME/.config/mimeapps.list   
-
-# script
-
-ln -s $HOME/.config/dot/script/b           $HOME/.config/i3              
+ln -s $HOME/.config/dot/script/b       $HOME/.config/i3              
